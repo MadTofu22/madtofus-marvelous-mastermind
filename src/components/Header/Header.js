b3006f96ef9a84e95aee54a6b154f0d66da5782d
MadTofu22 <stutler.tom@gmail.com>
2020-11-19 15:29:13 -0600

    added some profile images in assets for testing, is fully functional

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,22 +1,44 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import mapStoreToProps from '../../redux/mapStoreToProps';
+import {withRouter} from 'react-router';
 import './Header.css';
 
 // Custom components
 import NavBar from '../NavBar/NavBar';
 import UnregisteredUserWidget from '../UnregisteredUserWidget/UnregisteredUserWidget';
+import RegisteredUserWidget from "../RegisteredUserWidget/RegisteredUserWidget";
 
 class Header extends Component {
 
 	render() {
 		return (
 			<section className='headerWrapper'>
-				<UnregisteredUserWidget />
+				{console.log(this)}
+				{this.props.store.user.id ?
+				<>
+					<img 
+						className='headerAvatar'
+						src={this.props.store.user.avatar_url}
+						alt='User profile avatar'
+						onClick={() => this.props.history.push(`/profile/${this.props.store.user.username}`)}
+					/>
+					<RegisteredUserWidget />
+				</>
+				:
+				<>
+					<img 
+						className='headerAvatar'
+						src={'/assets/images/default_avatar.jpg'}
+						alt='User profile avatar'
+					/>
+					<UnregisteredUserWidget />
+				</>
+				}
 				<NavBar />
 			</section>
 		);
 	}
 }
 
-export default connect(mapStoreToProps)(Header);
+export default withRouter(connect(mapStoreToProps)(Header));
--- src/components/RegisteredUserWidget/RegisteredUserWidget.js
+++ src/components/RegisteredUserWidget/RegisteredUserWidget.js
@@ -1,1 +1,23 @@
-                    
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import mapStoreToProps from "../../redux/mapStoreToProps";
+import { withRouter } from "react-router";
+
+class RegisteredUserWidget extends Component {
+
+    render () {
+        return (
+          <div className="logoutContainer">
+            <p className="headerUsername">{this.props.store.user.username}</p>
+            <button
+              className="headerWidgetButton"
+              onClick={() => this.props.dispatch({type: 'LOGOUT'})}
+            >
+              Logout
+            </button>
+          </div>
+        );
+    }
+}
+
+export default withRouter(connect(mapStoreToProps)(RegisteredUserWidget));
