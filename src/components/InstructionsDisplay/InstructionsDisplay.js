0887c50f8af6fea70665d07df2daf4932ee3b862
MadTofu22 <stutler.tom@gmail.com>
2020-11-29 18:07:50 -0600

    how to play finished

--- src/components/InstructionsDisplay/InstructionsDisplay.js
+++ src/components/InstructionsDisplay/InstructionsDisplay.js
@@ -9,25 +9,32 @@ class InstructionsDisplay extends Component {
         return (
             <section id='instructionsDisplayWrapper'>
                 <div className='instructionsColumnContainer'>
-                    <h1>Game History</h1>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elit odio, cursus vitae gravida ut, efficitur mollis ligula. Nulla ornare vulputate iaculis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce at ipsum ac libero tincidunt tincidunt ac sit amet leo. Duis sem ante, lobortis et ex ac, mollis porta quam. Vestibulum quis justo sit amet erat convallis tempus. Curabitur feugiat justo vel felis tincidunt luctus. Nam eget tortor quis nisl blandit blandit. Vestibulum et neque ut nisl placerat iaculis sed eu augue. Duis sed lectus malesuada, vulputate lorem id, interdum nunc. Curabitur fringilla facilisis justo, non accumsan nibh varius sit amet. Suspendisse cursus vel ipsum quis suscipit. Proin eu felis a nisl semper varius. Pellentesque pretium mi ac velit eleifend, vel auctor tortor aliquam. Etiam et lacus urna. Proin vitae tristique purus.</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean elit odio, cursus vitae gravida ut, efficitur mollis ligula. Nulla ornare vulputate iaculis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Fusce at ipsum ac libero tincidunt tincidunt ac sit amet leo. Duis sem ante, lobortis et ex ac, mollis porta quam. Vestibulum quis justo sit amet erat convallis tempus. Curabitur feugiat justo vel felis tincidunt luctus. Nam eget tortor quis nisl blandit blandit. Vestibulum et neque ut nisl placerat iaculis sed eu augue. Duis sed lectus malesuada, vulputate lorem id, interdum nunc. Curabitur fringilla facilisis justo, non accumsan nibh varius sit amet. Suspendisse cursus vel ipsum quis suscipit. Proin eu felis a nisl semper varius. Pellentesque pretium mi ac velit eleifend, vel auctor tortor aliquam. Etiam et lacus urna. Proin vitae tristique purus.</p>
-                </div>
-                <div className='instructionsColumnContainer'>
-                    <h1>Game Instructions</h1>
+                    <h2>Game Instructions</h2>
+                    <h3>The goal of this game is to guess the hidden code of 4 color marbles. You have 8 total Guesses to match the hidden code!</h3>
                     <ol>
-                        <li>Instruction Text Here</li>
-                        <li>Instruction Text Here</li>
-                        <li>Instruction Text Here</li>
-                        <li>Instruction Text Here</li>
-                        <li>Instruction Text Here</li>
-                        <li>Instruction Text Here</li>
-                        <li>Instruction Text Here</li>
-                        <li>Instruction Text Here</li>
-                        <li>Instruction Text Here</li>
-                        <li>Instruction Text Here</li>
-                        <li>Instruction Text Here</li>
+                        <li>Select one of the 6 marbles from the Marble Bucket on the right.</li>
+                        <li>Add the marble to one of the 4 guess slots in the top most row of the Guesses section.</li>
+                        <li>When all 4 slots are filled for the first guess row click on the Check Guess button below the Marble Bucket.</li>
+                        <li>Check the Guess Results!
+                            <ul>
+                                <li>
+                                    Each red dot indicates a marble is in the correct slot.
+                                </li>
+                                <li>
+                                    Each white dot inicates a marbles color is correct, but not in the correct spot.
+                                </li>
+                            </ul>
+                        </li>
+                        <li>Repeat steps 1-4 until all 8 Guesses have been attempted, or you get the correct code!</li>
+                        <ul>
+                            <li>
+                                If you are a logged in User, your win/loss record will be updated to reflect this game.
+                            </li>
+                        </ul>
                     </ol>
+
+                    <h4>At any time you may select New Game to start over.</h4>
+                    
                 </div>
             </section> 
         );
